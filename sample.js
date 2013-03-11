c543923e3663d85b7c6f648657704670737e356e
Nicolas Mercier-Gaboury <nicolas@extrabacon.net>
2013-03-11 10:52:24 -0400

    added a readme.md file

---
+++ sample.js
@@ -0,0 +1,31 @@
+var xlrd = require('xlrd');
+
+xlrd.parse('myfile.xlsx', function (err, workbook) {
+	// Iterate on sheets
+	workbook.sheets.forEach(function (sheet) {
+		console.log('sheet: ' + sheet.name);
+		// Iterate on rows
+		sheet.rows.forEach(function (row) {
+			// Iterate on cells
+			row.forEach(function (cell) {
+				console.log(cell.address + ': ' + cell.value);
+			});
+		});
+	});
+});
+
+xlrd.stream('myfile.xlsx').on('open', function (workbook) {
+	console.log('successfully opened ' + workbook.file);
+}).on('data', function (data) {
+
+	var currentWorkbook = data.workbook,
+		currentSheet = data.sheet,
+		rows = data.rows;
+
+	// TODO: handle streaming logic here
+
+}).on('error', function (err) {
+	// TODO: handle error here
+}).on('close', function () {
+	// TODO: finishing logic here
+});
